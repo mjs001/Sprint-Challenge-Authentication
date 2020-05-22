5a7ea7d1484036ee893b1b316b3c06be5135aab7
miriam-sullivan <maljayd42@gmail.com>
2020-05-22 10:56:53 -0500

    got the routes set up moving onto middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,59 @@
-const router = require('express').Router();
-
-router.post('/register', (req, res) => {
-  // implement registration
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../jokes/jokes-model.js");
+const { isValid } = require("../jokes/jokes-service.js");
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+    credentials.password = hash;
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (isValid(req.body)) {
+    Users.findBy({ "u.username": username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = createToken(user);
+          res.status(200).json({ message: "Checkout our dad jokes!", token });
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password",
+    });
+  }
 });
 
+function createToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+  };
+  const secret = process.env.JWT_SECRET || "supersecretysecret";
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, secret, options);
+}
 module.exports = router;
---
+++ jokes/jokes-model.js
@@ -0,0 +1,27 @@
+const db = require("../database/connection.js");
+module.exports = {
+    add,
+    findById,
+    findBy
+};
+
+function add(user){
+    try{
+        const [id] = await db("users").insert(user, "id")
+        return findById(id)
+    }catch (error) {
+        throw error
+    }
+}
+
+function findById(id) {
+    return db("users").where({id}).first();
+}
+
+function findBy(filter) {
+    console.log("filter", filter);
+    return db("users as u")
+      .where(filter)
+      .select("u.id", "u.username", "u.password")
+      .orderBy("u.id");
+  }
